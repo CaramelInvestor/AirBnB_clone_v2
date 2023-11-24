4e953da401b1521cc802d9b672a396863097b0e3
CaramelInvestor <caramelInvestor@gmail.com>
2023-11-24 21:31:54 +0100

    Web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""
+This module starts a Flask web application
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """This returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
